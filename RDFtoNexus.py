e886eb9bdf38db38966f1725fbe4ffe3b1888d6f
jcgood <jcgood@buffalo.edu>
2013-07-11 14:57:52 -0400

    Updating the ontology.

--- RDFtoNexus.py
+++ RDFtoNexus.py
@@ -8,7 +8,7 @@ from tdag import simUI_d, get_distances, to_nex, draw_graphs, full_grid, process
 rdfTemplates = rdfGraph()
 
 instanceNS = Namespace("http://purl.org/linguistics/jcgood/template#")
-rdfTemplates.load("/Volumes/Obang/MyDocuments/Linearity/template_ontology/template.rdf")
+rdfTemplates.load("./template.rdf")
 
 # Get template IDs from RDF
 templatesGenerator = rdfTemplates.subjects(RDF.type, instanceNS['desmeme'])
@@ -20,7 +20,7 @@ for template in templatesGenerator:
 gTemplates = process_templates(templates, rdfTemplates)
 
 distances = get_distances(gTemplates)
-outfile = "/Volumes/Obang/MyDocuments/Linearity/template_ontology/template.nex"
+outfile = "/Volumes/Obang/MyDocuments/Linearity/TemplatesBook/template.nex"
 to_nex(distances,outfile)
 
 print full_grid(distances)
