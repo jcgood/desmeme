3dd9740491e80496fd40133c8a2f62ecd83609b2
jcgood <jcgood@buffalo.edu>
2013-08-01 11:25:10 -0400

    Adding in logic for templatic comparison without components, updated database.

--- RDFtoNexus.py
+++ RDFtoNexus.py
@@ -2,7 +2,7 @@
 
 import tdag
 from tdag import rdfGraph, Namespace, RDF, tdag, conflate, process_template, prettyName
-from tdag import simUI_d, get_distances, to_nex, draw_graphs, full_grid, process_templates
+from tdag import simUI_d, get_distances, to_nex, draw_graphs, full_grid, process_templates, process_templates_noComp
 
 
 rdfTemplates = rdfGraph()
@@ -18,9 +18,15 @@ for template in templatesGenerator:
 	templates.append(template)
 			
 gTemplates = process_templates(templates, rdfTemplates)
+gTemplatesNoComp = process_templates_noComp(templates, rdfTemplates)
 
 distances = get_distances(gTemplates)
 outfile = "/Volumes/Obang/MyDocuments/Linearity/TemplatesBook/template.nex"
 to_nex(distances,outfile)
+
+noCompDistances = get_distances(gTemplatesNoComp)
+noCompOutfile = "/Volumes/Obang/MyDocuments/Linearity/TemplatesBook/templateNoComp.nex"
+to_nex(noCompDistances,noCompOutfile)
+
 
 print full_grid(distances)
--- tdag/despecification.py
+++ tdag/despecification.py
@@ -47,6 +47,57 @@ def process_template(mother,genericMother,rdfGraph,tdag):
 	# It should be all built up by now. So, we return it.
 	return tdag
 
+# This takes in a regular template and outputs a despecified one, removing
+# RDF bits not important for the comparative analysis.
+# It's recursive and passes back the original nodes and a despecified/generic one.
+def process_template_noComp(mother,genericMother,rdfGraph,tdag):
+		
+	for predicate, daughter in rdfGraph.predicate_objects(mother):
+
+		# Conflate (if applicable) the daughters of the mother if possible and create pretty names.
+		despecifiedD = conflate(daughter,rdfGraph)
+		prettyDName = prettyName(despecifiedD)
+		prettyPredName = prettyName(predicate)
+
+		if despecifiedD.encode('utf-8') == "http://purl.org/linguistics/jcgood/component#component":
+			print "stopping"
+			
+		else:
+
+			# Here we need some special logic to make sure that (components especially)
+			# Have their potentially shared nodes "split" up so that each component points
+			# to its own nodes rather than shared nodes. This is only a problem for
+			# generic and/or conflatable nodes which may not be unique.
+			if is_generic(predicate,tdag) or conflatable(daughter,rdfGraph):
+
+				# I don't understand why but, somehow, the invocation of this boolean
+				# detects cases of duplicatable nodes not yet properly handled. This seems
+				# to be an accident, but a useful one. So, I am keeping it.
+				if tdag.has_node(prettyDName,daughter):
+					prettyDName = tdag.has_node(prettyDName,daughter)
+					print "Warning ", prettyDName, daughter, mother, "may be a case of a duplicatable node not yet properly handled. Examine the template using it and the class function in tdag and add a case for this if graph does not properly duplicate nodes."
+				else:
+					prettyDName = tdag.add_node(prettyDName,daughter,mother,predicate)
+			
+				# The method used here is from the original pygraph, and does not involve labeled edges
+				if tdag.has_edge((genericMother, prettyDName), prettyPredName):
+					pass
+				else:
+					# Now we are using the special tdag methods to add an edge with a label for multiple arcs from/to same nodes
+					tdag.add_edge((genericMother, prettyDName), label=prettyPredName)
+			
+				# Now send the rest of the template, after conflation of this layer, back through the function.
+				process_template_noComp(daughter, prettyDName, rdfGraph, tdag)
+			
+			# Not a generic/conflatable node. So, we don't add anything but, instead, just cycle through unless it's a component
+			else: process_template_noComp(daughter, genericMother, rdfGraph, tdag)
+	
+		
+	# We've been passing the same tdag around adding nodes and edges.
+	# It should be all built up by now. So, we return it.
+	return tdag
+
+
 
 # This function conflates/collapses an RDF node with its type if it is a typed node.
 # For instance, a specific FOUNDATION may become a type rather than a foundation ID.
@@ -104,6 +155,28 @@ def process_templates(templates, rdfTemplates):
 		templatesDAG.add_node(prettyT,template)
 	
 		despecifiedTemplate = process_template(template,prettyT,rdfTemplates,templatesDAG)
+
+		gTemplates.append(despecifiedTemplate)
+
+	return gTemplates
+	
+
+# Runs through a list of templates, to create a list of despecified templates
+def process_templates_noComp(templates, rdfTemplates):
+
+	gTemplates = [ ]
+
+	for template in templates:
+
+		tname = prettyName(template)
+		templatesDAG = tdag(tname)
+
+		# Beginning of template is special case--then we can process them by tree
+		genericT = conflate(template,rdfTemplates)
+		prettyT = prettyName(genericT)
+		templatesDAG.add_node(prettyT,template)
+	
+		despecifiedTemplate = process_template_noComp(template,prettyT,rdfTemplates,templatesDAG)
 
 		gTemplates.append(despecifiedTemplate)
 
