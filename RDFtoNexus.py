975480c9d204ac86151127303fad3320194bf349
jcgood <jcgood@buffalo.edu>
2013-11-19 15:03:36 -0500

    Automating pairwise distance to LaTeX.

--- RDFtoNexus.py
+++ RDFtoNexus.py
@@ -34,3 +34,92 @@ print "NoComp"
 
 distancesNoComp = get_distances(gTemplatesNoComp)
 print full_grid(distancesNoComp)
+
+
+# As long as here, let's generate some LaTeX--probably should put this in the module, but I'm too lazy right now
+pairwiseComps = open("/Volumes/Obang/MyDocuments/Linearity/TemplatesBook/pairwiseComps.tex", "w")
+print >> pairwiseComps, "\\begin{sidewaystable}[ht!]"
+print >> pairwiseComps, "{\\footnotesize"
+print >> pairwiseComps, "\\setlength{\\tabcolsep}{.45em}"
+print >> pairwiseComps, "\\begin{tabular}{@{}lrrrrrrrrrrrrrrrrrrrr}"
+
+distkeys = distances.keys()
+distkeys.sort()
+templateCount =  0
+for template in distkeys:
+	
+
+	tempDistances = distances[template]
+
+	distanceString = ""
+	gridCount = 0
+	tempdistkeys = tempDistances.keys()
+	tempdistkeys.sort()
+
+	for comparisonTemplate in tempdistkeys:
+
+		# Prints half grid
+		if gridCount > templateCount:
+			break
+			
+		else:
+			roundedDistance = '{0:.2f}'.format(round(tempDistances[comparisonTemplate],2))
+			distanceString += ("\t&\t" + str(roundedDistance))
+			
+		gridCount  += 1
+
+	# Write out the distances string
+	print >> pairwiseComps, template + distanceString + "\t\\\\"
+	templateCount += 1
+		
+print >> pairwiseComps, "\\end{tabular}}"
+print >> pairwiseComps, "\\caption{Pairwise distances among examined templates including component nodes \\label{Distances}}"
+print >> pairwiseComps, "\\end{sidewaystable}"
+
+pairwiseComps = open("/Volumes/Obang/MyDocuments/Linearity/TemplatesBook/pairwiseComps.tex", "w")
+print >> pairwiseComps, "\\begin{sidewaystable}[ht!]"
+print >> pairwiseComps, "{\\footnotesize"
+print >> pairwiseComps, "\\setlength{\\tabcolsep}{.45em}"
+print >> pairwiseComps, "\\begin{tabular}{@{}lrrrrrrrrrrrrrrrrrrrr}"
+
+
+# Now no comps distances
+pairwiseNoComps = open("/Volumes/Obang/MyDocuments/Linearity/TemplatesBook/pairwiseNoComps.tex", "w")
+print >> pairwiseNoComps, "\\begin{sidewaystable}[ht!]"
+print >> pairwiseNoComps, "{\\footnotesize"
+print >> pairwiseNoComps, "\\setlength{\\tabcolsep}{.45em}"
+print >> pairwiseNoComps, "\\begin{tabular}{@{}lrrrrrrrrrrrrrrrrrrrr}"
+
+distkeys = distancesNoComp.keys()
+distkeys.sort()
+templateCount =  0
+for template in distkeys:
+	
+
+	tempDistances = distances[template]
+
+	distanceString = ""
+	gridCount = 0
+	tempdistkeys = tempDistances.keys()
+	tempdistkeys.sort()
+
+	for comparisonTemplate in tempdistkeys:
+
+		# Prints half grid
+		if gridCount > templateCount:
+			break
+			
+		else:
+			roundedDistance = '{0:.2f}'.format(round(tempDistances[comparisonTemplate],2))
+			distanceString += ("\t&\t" + str(roundedDistance))
+			
+		gridCount  += 1
+
+	# Write out the distances string
+	print >> pairwiseNoComps, template + distanceString + "\t\\\\"
+	templateCount += 1
+		
+print >> pairwiseNoComps, "\\end{tabular}}"
+print >> pairwiseNoComps, "\\caption{Pairwise distances among examined templates excluding component nodes \\label{Distances}}"
+print >> pairwiseNoComps, "\\end{sidewaystable}"
+
