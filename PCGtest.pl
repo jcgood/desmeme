43dc4a4c9d023af2708f8414fdeb9fde77c9bbf6
jcgood <jcgood@buffalo.edu>
2013-11-21 15:04:57 -0500

    Working on PCG.

--- PCGtest.pl
+++ PCGtest.pl
@@ -15,14 +15,8 @@ use Moose;
 # $g2->add_edge_by_id("Emu", "Bamboo", "at-risk");
 
 
-
 my $g1 = Graph->new(multiedged => 1);
 $g1->set_graph_attribute("name", "ChichewaAR");
-$g1->add_edge_by_id("desmeme", "order", "STRICTURE");
-$g1->add_edge_by_id("order", "morphologicalConstituent", "CONSTITUENT");
-$g1->add_edge_by_id("order", "simple", "RELATIONS");
-$g1->add_edge_by_id("order", "COUNT2-1", "COUNT");
-$g1->add_edge_by_id("desmeme", "arch", "FOUNDATION");
 $g1->add_edge_by_id("arch", "component1", "LEFT_SUPPORT");
 $g1->add_edge_by_id("component1", "inelastic1", "ELASTICITY");
 $g1->add_edge_by_id("inelastic1", "COUNT1-1", "COUNT");
@@ -34,37 +28,9 @@ $g1->add_edge_by_id("component2", "inelastic2", "ELASTICITY");
 $g1->add_edge_by_id("inelastic2", "COUNT0-1", "COUNT");
 $g1->add_edge_by_id("component2", "null1", "FILLEDNESS");
 $g1->add_edge_by_id("component2", "stable2", "STABILITY");
-$g1->add_edge_by_id("arch", "component3", "RIGHT_VOUSSOIR");
-$g1->add_edge_by_id("component3", "elastic1", "ELASTICITY");
-$g1->add_edge_by_id("elastic1", "MINIMUM0-1", "MINIMUM");
-$g1->add_edge_by_id("elastic1", "MAXIMUM1-1", "MAXIMUM");
-$g1->add_edge_by_id("component3", "filled1", "FILLEDNESS");
-$g1->add_edge_by_id("filled1", "canonicalLineate1", "FORM");
-$g1->add_edge_by_id("component3", "stable3", "STABILITY");
-$g1->add_edge_by_id("arch", "component1", "KEYSTONE");
-$g1->add_edge_by_id("arch", "component4", "RIGHT_SUPPORT");
-$g1->add_edge_by_id("component4", "elastic2", "ELASTICITY");
-$g1->add_edge_by_id("elastic2", "MINIMUM0-2", "MINIMUM");
-$g1->add_edge_by_id("elastic2", "MAXIMUM1-2", "MAXIMUM");
-$g1->add_edge_by_id("component4", "filled2", "FILLEDNESS");
-$g1->add_edge_by_id("filled2", "canonicalLineate2", "FORM");
-$g1->add_edge_by_id("component4", "stable4", "STABILITY");
-$g1->add_edge_by_id("desmeme", "lexicoconstructionalConditioning", "CONDITIONING");
-$g1->add_edge_by_id("lexicoconstructionalConditioning", "filledComponentSet", "FILLED_COMPONENTS");
-$g1->add_edge_by_id("filledComponentSet", "component3", "FILLED_COMPONENT");
-$g1->add_edge_by_id("filledComponentSet", "component4", "FILLED_COMPONENT");
-$g1->add_edge_by_id("lexicoconstructionalConditioning", "multiple", "FILLER_POSITION");
-$g1->add_edge_by_id("desmeme", "potentiallyViolable", "VIOLABILITY");
-$g1->add_edge_by_id("potentiallyViolable", "morphosyntacticInsertion", "REPARABILITY");
-$g1->add_edge_by_id("potentiallyViolable", "semantic", "EXCEPTIONALITY");
+
 
 my $g2 = Graph->new(multiedged => 1);
-$g2->set_graph_attribute("name", "MeskwakiClause");
-$g2->add_edge_by_id("desmeme", "order", "STRICTURE");
-$g2->add_edge_by_id("order", "syntacticConstituent", "CONSTITUENT");
-$g2->add_edge_by_id("order", "simple", "RELATIONS");
-$g2->add_edge_by_id("order", "COUNT5-1", "COUNT");
-$g2->add_edge_by_id("desmeme", "arch", "FOUNDATION");
 $g2->add_edge_by_id("arch", "component1", "LEFT_SUPPORT");
 $g2->add_edge_by_id("component1", "elastic1", "ELASTICITY");
 $g2->add_edge_by_id("elastic1", "MINIMUM0-1", "MINIMUM");
@@ -79,32 +45,6 @@ $g2->add_edge_by_id("inelastic1", "COUNT1-1", "COUNT");
 $g2->add_edge_by_id("component2", "open2", "FILLEDNESS");
 $g2->add_edge_by_id("open2", "coherent2", "COHERENCE");
 $g2->add_edge_by_id("component2", "stable1", "STABILITY");
-$g2->add_edge_by_id("arch", "restkomponentenSet", "RESTKOMPONENTEN");
-$g2->add_edge_by_id("restkomponentenSet", "component3", "RESTKOMPONENT");
-$g2->add_edge_by_id("component3", "elastic2", "ELASTICITY");
-$g2->add_edge_by_id("elastic2", "MINIMUM0-2", "MINIMUM");
-$g2->add_edge_by_id("elastic2", "MAXIMUM1-2", "MAXIMUM");
-$g2->add_edge_by_id("component3", "open3", "FILLEDNESS");
-$g2->add_edge_by_id("open3", "coherent3", "COHERENCE");
-$g2->add_edge_by_id("component3", "stable2", "STABILITY");
-$g2->add_edge_by_id("arch", "component4", "LEFT_VOUSSOIR");
-$g2->add_edge_by_id("component4", "elastic3", "ELASTICITY");
-$g2->add_edge_by_id("elastic3", "MINIMUM0-3", "MINIMUM");
-$g2->add_edge_by_id("elastic3", "MAXIMUM1-3", "MAXIMUM");
-$g2->add_edge_by_id("component4", "open4", "FILLEDNESS");
-$g2->add_edge_by_id("open4", "coherent4", "COHERENCE");
-$g2->add_edge_by_id("component4", "stable3", "STABILITY");
-$g2->add_edge_by_id("arch", "component5", "RIGHT_VOUSSOIR");
-$g2->add_edge_by_id("component5", "elastic4", "ELASTICITY");
-$g2->add_edge_by_id("elastic4", "MINIMUM0-4", "MINIMUM");
-$g2->add_edge_by_id("elastic4", "MAXIMUM100-1", "MAXIMUM");
-$g2->add_edge_by_id("component5", "open5", "FILLEDNESS");
-$g2->add_edge_by_id("open5", "coherent5", "COHERENCE");
-$g2->add_edge_by_id("component5", "stable4", "STABILITY");
-$g2->add_edge_by_id("arch", "component2", "KEYSTONE");
-$g2->add_edge_by_id("arch", "component5", "RIGHT_SUPPORT");
-$g2->add_edge_by_id("desmeme", "constructionalConditioning", "CONDITIONING");
-$g2->add_edge_by_id("desmeme", "notViolable", "VIOLABILITY");
 
 
 my $pcg = Graph->new(multiedged => 1);
@@ -171,6 +111,7 @@ print DOT  "gr = tdag.tdag(\"\")\n\n";
 my $printgraph = $pcg;
 my @alledges = $printgraph->edges();
 
+my %seenEdges;
 for my $edgeArray (@alledges) {
 
 	my ($n1, $n2) = @{$edgeArray};
@@ -182,7 +123,10 @@ for my $edgeArray (@alledges) {
 		print DOT  "except: pass\n";
 		print DOT  "try: gr.add_node(\"$n2\", \"\")\n";
 		print DOT  "except: pass\n\n";
-		print DOT  "gr.add_edge((\"$n1\", \"$n2\"), label=\"$edgeLabel\")\n\n";
+		if (exists($seenEdges{"$n2/$n1"})) { print DOT  "gr.add_edge((\"$n1\", \"$n2\"), label=\" \")\n\n"; print "hellp\n";}
+		else {print "$n2/$n1 ".exists($seenEdges{"$n2/$n1"})."\n"; }
+		$seenEdges{"$n1/$n2"} = 1;
+		
 		}
 	
 	}
