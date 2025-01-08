24efab91055917d51eb942c4067b74a65c8d8362
jcgood <jcgood@buffalo.edu>
2025-01-07 20:38:53 -0500

    added component schema

--- ReadDesmemeSchema.py
+++ ReadDesmemeSchema.py
@@ -1,10 +1,11 @@
 from tdag.validator import schema
 
 #schemaFileName = "DesmemeSchema.tsv"
-schemaFileName = "CmponentSchema.tsv"
+schemaFileName = "ComponentSchema.tsv"
 
 #desmemeSchema = schema(schemaFileName, "desmeme")
 desmemeSchema = schema(schemaFileName, "component")
 
 [typesToFeatures, featuresToTypes] = desmemeSchema.processSchema()
 
+print(typesToFeatures,"\n\n",featuresToTypes)
